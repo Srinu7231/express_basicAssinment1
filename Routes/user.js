d87bb254434bdae6f2ac38e4e5b1d4271ab752c1
srinu <doddaeswararao@gmail.com>
2024-03-24 08:47:30 +0530

    Template Engines

--- Routes/user.js
+++ Routes/user.js
@@ -1,12 +1,22 @@
-const express = require('express')
-const router = express.Router()
-const path = require('path')
-const directory = require('../path')
-router.get('/',(req,res,next)=>{
-    res.send('<h1>Main root ..........</h1>')
-})
-router.get('/users',(req,res,next)=>{
-    res.sendFile(path.join(directory,'Views','user.html'))
-})
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const directory = require("../path");
+let users = [];
+router.get("/", (req, res, next) => {
+  // res.send("<h1>Main root ..........</h1>");
+  console.log(req?.body);
+  res.render("addUser", { pageTitle: "add user" });
+});
+router.post("/", (req, res, next) => {
+  console.log(req?.body);
+  users.push({ name: req?.body?.name });
+  res.redirect("/users");
+});
 
-module.exports = router;
+// router.get("/users", (req, res, next) => {
+//   // res.sendFile(path.join(directory,'Views','user.html'))
+//   res.render("user.ejs", { pageTitle: "users" });
+// });
+exports.routes = router;
+exports.users = users;
--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const express = require('express')
-const http = require('http')
-const app = express()
-const userRoute = require('./Routes/user')
-const directory = require('./path') 
-const path = require('path')
+const express = require("express");
+const http = require("http");
+const app = express();
+const userRoute = require("./Routes/user");
+const directory = require("./path");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-// we were using user route here 
-app.use(userRoute)
-app.use(express.static(path.join(directory,'JSCODES')))
-app.use((res,req,next)=>{
-    console.log(res.statusCode)
-    req.status(404).sendFile(path.join(directory,'Views','404page.html'))
-})
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const server = http.createServer(app)
-app.listen(3000)
+app.use(bodyParser.urlencoded());
+console.log("13 ================>", userRoute.users);
+// we were using user route here
+app.use(userRoute.routes);
+app.use(express.static(path.join(directory, "JSCODES")));
+app.get("/users", (req, res, next) => {
+  // res.sendFile(path.join(directory,'Views','user.html'))
+  res.render("user.ejs", { pageTitle: "users", users: userRoute.users });
+});
+app.use((res, req, next) => {
+  console.log(res.statusCode);
+  // req.status(404).sendFile(path.join(directory,'Views','404page.html'))
+  req.status(404).render("404", { pageTitle: "404 Error" });
+});
+
+const server = http.createServer(app);
+app.listen(3000);
